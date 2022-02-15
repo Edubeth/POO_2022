4d4008aecdf2de699aa03974583788670a5c7c9e
Eduardo <76177427+Edubeth@users.noreply.github.com>
2022-02-15 14:56:34 -0600

    Update Main.java

--- src/main/java/Generic/Main.java
+++ src/main/java/Generic/Main.java
@@ -2,13 +2,10 @@ package Generic;
 
 public class Main {
     public static void main (String [] args) {
-        GenericTupla<Integer, String> point1 = new GenericTupla<>(28 "Elizabeth");
-        GenericTupla<Integer, String> point2 = new GenericTupla<>(28 "Elizabeth");
-        ObjectTupla objTuple = new ObjectTupla(21, "Edubeth
+        GenericTupla<Integer, String> point1 = new GenericTupla<>(28, "Elizabeth");
+        GenericTupla<Integer, String> point2 = new GenericTupla<>(28, "Elizabeth");
+        ObjectTupla objTuple = new ObjectTupla(21, "Edubeth");
         boolean isEqual = Utilities.compare(point1, point2);
-        System.out.println(point1);
-        System.out.println(point2);
-        System.out.println(objTuple);
         System.out.println("Comparacion de tuplas con el metodo equals: " + point1.equals(point2));
         System.out.println("Comparacion de tuplas con la clase utilities: " + isEqual);
         System.out.println("Comparacion de tupla point2 y objTuple: " + Utilities.compare(point2, objTuple));
